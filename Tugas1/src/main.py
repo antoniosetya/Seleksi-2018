c8c166e94d1ba774ef3cf9c7ba1f55d477ff4556
Antonio Setya <antonio_setya@hotmail.com>
2018-05-14 22:54:06 +0700

    Currently working (but flawed) scraper
    Partial data is retrieved, more to come.

--- Tugas1/src/main.py
+++ Tugas1/src/main.py
@@ -1,4 +1,6 @@
 import urllib.request
+import traceback
+import time
 from bs4 import BeautifulSoup
 import json
 
@@ -9,7 +11,15 @@ pokemons = []
 state = {}
 
 def CleanDexNmbr(nmbr):
-    return nmbr[2:5]
+    return int(nmbr[2:5])
+
+def IsNmbrInList(data,number):
+    found = False
+    for elm in data:
+        if (elm["no"] == number):
+            found = True
+            break
+    return found
 
 def GetInitList():
     # Grabbing initial Pokemon data (list of all Pokemon)
@@ -35,7 +45,6 @@ def GetInitList():
     # with 'Ndex' inside the <th> element
 
     print("Parsing data...")
-    c = 0
     for tbl in tables:
         identifier = tbl.find_all("th")
         if identifier:
@@ -48,9 +57,6 @@ def GetInitList():
                 correct = False
                 pass
             if correct:
-                # Grabs region of Dex
-                temp_reg = region_code[c]
-                c = c + 1
                 # Grabs initial Pokemon data
                 pkmns = tbl.find_all("tr",style="background:#FFF")
                 for rows in pkmns:
@@ -59,12 +65,126 @@ def GetInitList():
                     name = datas[3].a.string
                     ext_link = "https://bulbapedia.bulbagarden.net" + datas[3].a.get("href")
                     # Adds data to the list
-                    pokemon_list.append({"no" : number, "name" : name, "link" : ext_link})
+                    if not IsNmbrInList(pokemon_list,int(number)):
+                        pokemon_list.append({"no" : number, "name" : name, "link" : ext_link})
 
     # Dumps data to JSON file
     with open(data_dir + "init_list.json","w") as f:
         json.dump(pokemon_list,f)
     pokemons = pokemon_list
+
+def SingleScrape(index):
+    global pokemons
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+           }
+    req = urllib.request.Request(pokemons[index]["link"],headers=hdr)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        soup = BeautifulSoup(html,'html.parser')
+    main_data = soup.find("div",attrs={"id" : "mw-content-text"})
+    metadata = main_data.find("table",attrs={"class" : "roundy"}).find_all("td",attrs={"class" : "roundy"})
+    # Determines whether this Pokemon has alternate forms
+    # LATERRR
+
+    # Extracts type data
+    meta_type = (metadata[1].find_all("table"))[1].find_all("td")
+    type = []
+    for td in meta_type:
+        type.append(td.find("span").find("b").contents[0])
+    #print(type)
+
+    # Extracts abilities
+    meta_ability = metadata[2].find_all("td")
+    ability = []
+    for td in meta_ability:
+        if not td.has_attr("style"):
+            ability.append(td.find("a").find("span").contents[0])
+    #print(ability)
+
+    # Extracts height and weight
+    meta_h = metadata[6]
+    meta_w = metadata[7]
+    height = (((meta_h.find_all("tr"))[0].find_all("td")[1].contents[0])[1:])[:-1]
+    weight = (((meta_w.find_all("tr"))[0].find_all("td")[1].contents[0])[1:])[:-1]
+    #print(height + " | " + weight)
+
+    # Extracts when this Pokemon is introduced
+    when_str = (main_data.find_all("table",attrs={"class" : "roundy"}))[23].find("th").find("small").contents[0]
+    #print(when_str)
+
+    # Extracts base stats of this Pokemon
+    try:
+        stat_table = main_data.find("span",attrs={"id" : "Base_stats"}).next_element.next_element.next_element.next_element.next_element.next_element.next_element
+        stat_elements = stat_table.find_all("tr")
+        hp = ((stat_elements[2].find("table").find_all("th"))[1].contents[0])[1:-1]
+    except:
+        stat_table = main_data.find("span",attrs={"id" : "Base_stats"}).next_element.next_element.next_element
+        stat_elements = stat_table.find_all("tr")
+        hp = ((stat_elements[2].find("table").find_all("th"))[1].contents[0])[1:-1]
+        #print(hp)
+    atk = ((stat_elements[4].find("table").find_all("th"))[1].contents[0])[1:-1]
+    #print(atk)
+    defn = ((stat_elements[6].find("table").find_all("th"))[1].contents[0])[1:-1]
+    #print(defn)
+    spatk = ((stat_elements[8].find("table").find_all("th"))[1].contents[0])[1:-1]
+    #print(spatk)
+    spdef = ((stat_elements[10].find("table").find_all("th"))[1].contents[0])[1:-1]
+    #print(spdef)
+    spe = ((stat_elements[12].find("table").find_all("th"))[1].contents[0])[1:-1]
+    #print(spe)
+
+    # Inserting results into out array
+    out = {}
+    out["no"] = pokemons[index]["no"]
+    out["name"] = pokemons[index]["name"]
+    out["type"] = type
+    out["ability"] = ability
+    out["weight"] = weight
+    out["stats"] = {"hp" : hp, "atk" : atk, "def" : defn, "spatk" : spatk, "spdef" : spdef, "spe" : spe}
+    return out
+
+def Scrape(i,initial = []):
+    global pokemons
+    count = len(pokemons)
+    out = initial
+    not_scrapped = []
+    curIndex = 0
+    try:
+        for now in range(i,count):
+            try:
+                print("Scraping " + pokemons[now]["name"] + " (#" + str(pokemons[now]["no"]) + ")...")
+                out.append(SingleScrape(now))
+                print(pokemons[now]["name"] + " (#" + str(pokemons[now]["no"]) + ") scrapped")
+            except KeyboardInterrupt:
+                print("Ctrl-C pressed, stopping...")
+                break
+            except:
+                traceback.print_exc()
+                print("Error in scraping " + pokemons[now]["name"] + " (#" + str(pokemons[now]["no"]) + "), skipping...")
+                not_scrapped.append(now)
+            finally:
+                time.sleep(0.5)
+    except KeyboardInterrupt:
+        print("Ctrl-C pressed, stopping...")
+        pass
+    state = {}
+    state["lastScrapped"] = now
+    state["notScrapped"] = not_scrapped
+    try:
+        with open(data_dir + "main_data.json","r") as f:
+            prev_data = json.load(f)
+    except:
+        prev_data = []
+    out = prev_data + out
+    # Sort data based on Dex number
+    out.sort(key=lambda x:x["no"])
+    with open(data_dir + "state.json","w") as f:
+        json.dump(state,f)
+    with open(data_dir + "main_data.json","w") as f:
+        json.dump(out,f)
+    print("\n\n")
+    print("Saved " + str(len(out)) + " entries with " + str(len(not_scrapped)) + " entries skipped.")
 
 # Trying to load initial data
 try:
@@ -81,7 +201,10 @@ if not pokemons:
     GetInitList()
 
 # Trying to load last state of scraping, in case it is interrupted
+# Even though state.json exists, if main_data.json doesn't exists, it will ignore it
 try:
+    f = open(data_dir + "main_data.json","r")
+    f.close()
     with open(data_dir + "state.json","r") as f:
         state = json.load(f)
 except FileNotFoundError:
@@ -89,7 +212,7 @@ except FileNotFoundError:
 except JSONDecodeError:
     pass
 
-print("What do you want to do?")
+print("\nWhat do you want to do?")
 print("---------------------------------")
 print("1. Start scraping data.")
 print("2. Refresh initial data.")
@@ -103,13 +226,30 @@ valid = False
 while not valid:
     sel = input("Your choice : ")
     if (sel == "1"):
-        # Start scraping
+        print("Scraping job started...!")
+        Scrape(0)
         valid = True
     elif (sel == "2"):
         GetInitList()
         valid = True
     elif ((sel == "R" or sel == "r") and state):
-        # start scraping
+        temp = []
+        if state["notScrapped"]:
+            print("Scrapping errored entries first... ")
+            while state["notScrapped"]:
+                current = state["notScrapped"].pop()
+                try:
+                    print("Scraping " + pokemons[current]["name"] + " (#" + str(pokemons[current]["no"]) + ")...")
+                    temp.append(SingleScrape(current))
+                    print(pokemons[current]["name"] + " (#" + str(pokemons[current]["no"]) + ") scrapped")
+                except KeyboardInterrupt:
+                    break
+                except:
+                    traceback.print_exc()
+                    print("Error in scraping " + pokemons[current]["name"] + " (#" + str(pokemons[current]["no"]) + "), will try later...")
+                    state["notScrapped"].append(current)
+        print("Resuming...")
+        Scrape(state["lastScrapped"],temp)
         valid = True
     elif (sel == "E" or sel == "e"):
         print("Buh bye...!")
